c49f7b7f5f590cefac677a15937792e91c40e101
Rubarajan <rubarajankcs@hotmail.com>
2020-04-02 10:18:22 +0530

    Testing Git Commit

--- NSETraderBot/NSETraderBot/Trader.py
+++ NSETraderBot/NSETraderBot/Trader.py
@@ -6,7 +6,7 @@ all_nse_stocks = nseHelper.getallNSEStockName()
 
 
 # print(all_nse_stocks)
-
+# Test_GitHub
 
 for stock_symbol in all_nse_stocks:
     print(f" the stock symbol is {stock_symbol} and the company is {stock_symbol[:]}")
