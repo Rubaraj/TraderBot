36ff6998f2cc81f48f716bc5e65397f6ae4b157d
Rubarajan <rubarajankcs@hotmail.com>
2020-04-15 12:10:07 +0530

    Added Max Reach

--- NSETraderBot/NSETraderBot/Tradingstrategy.py
+++ NSETraderBot/NSETraderBot/Tradingstrategy.py
@@ -19,7 +19,7 @@ class OpenHighOpenLow:
             <!DOCTYPE html> <html> <head> 
             <style> table, th, td {   border: 1px solid black; border-collapse: collapse; } th, td {   padding: 5px;   text-align: left; } th {background-color: #4CAF50;color: white;}</style>
             </head> <body> <h2>Eligible list of Open High and Open Low</h2>
-            <table style="width:100%"> <tr> <th>SYMBOL</th> <th> LAST TRADED PRICE </th> <th>OPEN</th> <th>HIGH</th> <th>LOW</th> <th>DAILYVOLATILITY</th> <th> VOLATILITYVALUE </th> <th>BUY/SELL</th>"""
+            <table style="width:100%"> <tr> <th>SYMBOL</th> <th> LAST TRADED PRICE </th> <th>OPEN</th> <th>HIGH</th> <th>LOW</th> <th> VOLATILITY VALUE (Rs) </th> <th> MAX REACH </th> <th>BUY/SELL</th>"""
 
             noteligiblecound = 0
             eligibletobuy = 0
@@ -28,22 +28,26 @@ class OpenHighOpenLow:
             for value in nify50_json_obj:
                 if value['open'] == value['high'] or value['open'] == value['low']:
                     stockderivativedetail = nseData.getOtherInformationNSEDerivative(value['symbol'])[0];
-                    volatilitycal = (float(stockderivativedetail['dailyVolatility']) / 100) * float(str(value['open']).replace(',',''))
+                    stock_open = value['open'].replace(',','')
+                    volatilitycal = (float(stockderivativedetail['dailyVolatility']) / 100) * float(stock_open)
                     mail_htmlcontent += f"""<tr>
                     <td>{value['symbol']}</td>
                     <td>{value['ltP']}</td>
                     <td>{value['open']}</td>
                     <td>{value['high']}</td>
                     <td>{value['low']}</td>
-                    <td>{stockderivativedetail['dailyVolatility']}</td>
                     <td>{round(volatilitycal,2)}</td>"""
 
                     if value['open'] == value['high']:
+                        eligibledown = float(stock_open) - round(volatilitycal,2)
                         print(f"Need to sell {value['symbol']}")
+                        mail_htmlcontent += f"<td>{round(eligibledown,2)}</td>"
                         mail_htmlcontent += "<td>SELL</td>"
                         eligibletosell += 1
                     if value['open'] == value['low']:
+                        eligibleup = float(stock_open) + round(volatilitycal,2)
                         print(f"Need to buy {value['symbol']}")
+                        mail_htmlcontent += f"<td>{round(eligibleup,2)}</td>"
                         mail_htmlcontent += "<td>BUY</td>"
                         eligibletobuy += 1
 
